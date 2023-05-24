d532d360bcff58219df2d829bd042ceb65dc2e21
CedricJ <106307333+SirAutism@users.noreply.github.com>
2023-05-24 11:21:20 +0200

    2 users added to FriendshipFixtures.php

--- src/DataFixtures/FriendshipFixtures.php
+++ src/DataFixtures/FriendshipFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,17 @@ class FriendshipFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        $user1 = new User();
+        $user1->setEmail($faker->email);
+        $user1->setPassword($faker->password);
+        $user1->setDisplayName($faker->name);
+
+        $user2 = new User();
+        $user2->setEmail($faker->email);
+        $user2->setPassword($faker->password);
+        $user2->setDisplayName($faker->name);
 
         $manager->flush();
     }
