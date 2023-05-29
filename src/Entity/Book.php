23d3120a8849713263178186be1231684e76384d
CedricJ <106307333+SirAutism@users.noreply.github.com>
2023-05-29 21:51:34 +0200

    Comment entity and repository added, altered Book and User entity with relations and stuff

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -18,17 +18,6 @@ class Book
     #[ORM\Column(length: 255)]
     private ?string $isbn = null;
 
-    #[ORM\Column(length: 255)]
-    private string $title;
-
-    #[ORM\OneToMany(mappedBy: 'Book', targetEntity: Comment::class)]
-    private Collection $comments;
-
-    public function __construct()
-    {
-        $this->comments = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -44,40 +33,5 @@ class Book
         $this->isbn = $isbn;
 
         return $this;
-    }
-
-    /**
-     * @return Collection<>
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-    public function addComment(Comment $comment): self
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments[] = $comment;
-            $comment->setBook($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): self
-    {
-        if ($this->comments->contains($comment)) {
-            $this->comments->removeElement($comment);
-            // set the owning side to null (unless already changed)
-            if ($comment->getBook() === $this) {
-                $comment->setBook(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getTitle(): string
-    {
-        return $this->title;
     }
 }
