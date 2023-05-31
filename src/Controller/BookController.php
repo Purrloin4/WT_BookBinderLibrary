08d7a020532b72f7d9de1332b029d90bbf89ddbd
Cédric <cedric.josse@student.kuleuven.be>
2023-05-31 11:37:43 +0200

    Show comments of book, remove wrong files

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,6 +6,7 @@ use App\Entity\Book;
 use App\Entity\Comment;
 use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,10 +34,26 @@ class BookController extends AbstractController
             $this->addFlash('success', 'Your comment was added!');
         }
 
+        $commentMessages = $this->getCommentMessages($book, $entityManager);
+
         return $this->render('book/index.html.twig', [
             'book' => $book,
+            'commentMessages' => $commentMessages,
             'commentForm' => $commentForm->createView(),
         ]);
+    }
+
+    public function getCommentMessages(Book $book, EntityManagerInterface $entityManager): array
+    {
+        $commentMessages = [];
+        $book_id = $book->getId();
+        $comments = $entityManager->getRepository(Comment::class)->findAll($book_id);
+        foreach ($comments as $comment_item) {
+            $commentMessages[] = $comment_item->getMessage();
+        }
+        // like here also add commenter name and date
+
+        return $commentMessages;
     }
 
     // Todo: edit and delete comment
