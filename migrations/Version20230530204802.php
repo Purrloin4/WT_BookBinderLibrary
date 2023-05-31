a6f1c9d89e6aaa5a1b434992a26bb991619480fb
YeWentai <yewentai126@gmail.com>
2023-05-31 09:45:00 +0200

    display comments on book page

--- migrations/Version20230530204802.php
+++ migrations/Version20230530204802.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230530100917 extends AbstractMigration
+final class Version20230530204802 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,13 +23,17 @@ final class Version20230530100917 extends AbstractMigration
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, commenter_id INT NOT NULL, book_id INT NOT NULL, message VARCHAR(255) NOT NULL, timestamp DATETIME NOT NULL, edited TINYINT(1) NOT NULL, INDEX IDX_9474526CB4D5A9E2 (commenter_id), INDEX IDX_9474526C16A2B381 (book_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CB4D5A9E2 FOREIGN KEY (commenter_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C16A2B381 FOREIGN KEY (book_id) REFERENCES book (id)');
+        $this->addSql('DROP TABLE book_isbn13');
+        $this->addSql('ALTER TABLE book ADD title VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE book_isbn13 (id INT DEFAULT NULL, isbn13 VARCHAR(13) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_0900_ai_ci`) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CB4D5A9E2');
         $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C16A2B381');
         $this->addSql('DROP TABLE comment');
+        $this->addSql('ALTER TABLE book DROP title');
     }
 }
