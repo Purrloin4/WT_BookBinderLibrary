3dea68a3e4314b75fa2737936cb0ff93011044b9
Dries.Van Ranst <driesvanranst94@hotmail.com>
2023-06-03 11:07:01 +0200

    Made a very basic test for the /messages page.

--- tests/FunctionalTests/MessagePageTest.php
+++ tests/FunctionalTests/MessagePageTest.php
@@ -2,6 +2,16 @@
 
 namespace App\Tests\FunctionalTests;
 
-class MessagePageTest
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MessagePageTest extends WebTestCase
 {
+    public function testMessagePageLoadsSuccessfully()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/messages');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h3', 'Recent chats');
+    }
 }
