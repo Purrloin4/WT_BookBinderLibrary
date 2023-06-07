efa23da4346c8d98e62909ffbb2240b5f97fa2a8
ZAMasterthesis <zeus.anseeuw@student.kuleuven.be>
2023-06-07 12:11:35 +0200

    Rename Follow entity to subscribe, updated Book entity, reverted changes to User entity, update migration file so all changes are added to db

---
+++ migrations/Version20230607100928.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230607100928 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book ADD published_date DATETIME NOT NULL, ADD average_rating DOUBLE PRECISION DEFAULT NULL, ADD ratings_count INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user DROP published_date, DROP average_rating, DROP ratings_count');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE book DROP published_date, DROP average_rating, DROP ratings_count');
+        $this->addSql('ALTER TABLE user ADD published_date DATETIME DEFAULT NULL, ADD average_rating DOUBLE PRECISION DEFAULT NULL, ADD ratings_count INT DEFAULT NULL');
+    }
+}
