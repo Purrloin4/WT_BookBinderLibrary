5b48fc8db0d75877b7fc5e6681ffee3cf2c7453d
Dries.Van Ranst <driesvanranst94@hotmail.com>
2023-06-10 14:25:12 +0200

    Wrote tests in the UserPageTest.php file.

--- tests/FunctionalTests/UserPageTest.php
+++ tests/FunctionalTests/UserPageTest.php
@@ -2,7 +2,21 @@
 
 namespace App\Tests\FunctionalTests;
 
-class UserPageTest
-{
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+class UserPageTest extends WebTestCase
+{
+    public function testUserPageLoadsSuccessfully()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/user/1');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('.account_info', 'My account info');
+        $this->assertSelectorTextContains('.user_name', 'Ariane Feest');
+        $this->assertSelectorTextContains('.user_email', 'hello1@world.org');
+        $this->assertSelectorTextContains('.user_friends', 'My Friends');
+        $this->assertSelectorTextContains('.user_comments', 'My Comments');
+        $this->assertSelectorTextContains('.user_messages', 'My Messages');
+    }
 }
